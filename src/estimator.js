f6e61b3aaed12295c8bce1c7e7ece2f5dc66ff42
Samuel Paintsil <smashbros54@gmail.com>
2020-04-12 22:23:01 +0000

    Challenge 1 test

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,21 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+
+  //Challenge 1
+
+
+
+  return {
+    data:input,
+    impact: {
+      currentlyInfected: input.reportedCases * 10,
+    },
+    severeImpact: {
+      currentlyInfected: input.reportedCases * 50
+    }
+  }
+};
 
 export default covid19ImpactEstimator;
+
+
