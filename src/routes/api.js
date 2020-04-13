2b3928e085990ef55fbe5c4f54d29ad6ade7aac8
Samuel Paintsil <smashbros54@gmail.com>
2020-04-13 11:22:04 +0000

    backend

---
+++ src/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const estimator = require('../estimator');
+
+const router = express.Router();
+const estimates = estimator.default();
+
+router.get('/', (req, res) => {
+  res.send(estimates);
+})
+
+module.exports = router;
