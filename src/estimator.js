17a2bf56f54574ef4fca161e9cbf116a256f4f5a
Samuel Paintsil <smashbros54@gmail.com>
2020-04-13 03:51:18 +0000

    Challenge 1-3 (1.7)

--- src/estimator.js
+++ src/estimator.js
@@ -18,7 +18,7 @@ const covid19ImpactEstimator = (data) => {
     }
     return mulFact;
   };
-  const factMulti = (Math.trunc(2 ** factor()));
+  const fM = (Math.trunc(2 ** factor()));
 
   // Challenge 3
   const pCal = () => {
@@ -41,27 +41,27 @@ const covid19ImpactEstimator = (data) => {
 
   const avgDIncome = input.region.avgDailyIncomeInUSD;
   const avgPIncome = input.region.avgDailyIncomePopulation;
-  const rCaseS = (input.reportedCases * 50);
+  const rS = (input.reportedCases * 50);
 
   return {
     data: input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
-      infectionsByRequestedTime: (input.reportedCases * 10) * factMulti,
-      severeCasesByRequestedTime: 0.15 * ((input.reportedCases * 10) * factMulti),
+      infectionsByRequestedTime: (input.reportedCases * 10) * fM,
+      severeCasesByRequestedTime: 0.15 * ((input.reportedCases * 10) * fM),
       hospitalBedsByRequestedTime: Math.trunc(0.35 * input.totalHospitalBeds),
-      casesForICUByRequestedTime: 0.05 * ((input.reportedCases * 10) * factMulti),
-      casesForVentilatorsByRequestedTime: 0.02 * ((input.reportedCases * 10) * factMulti),
-      dollarsInFlight: (((input.reportedCases * 10) * factMulti) * avgPIncome) * avgDIncome * pCal()
+      casesForICUByRequestedTime: 0.05 * ((input.reportedCases * 10) * fM),
+      casesForVentilatorsByRequestedTime: 0.02 * ((input.reportedCases * 10) * fM),
+      dollarsInFlight: (((input.reportedCases * 10) * fM) * avgPIncome) * avgDIncome * pCal()
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
-      infectionsByRequestedTime: (input.reportedCases * 50) * factMulti,
-      severeCasesByRequestedTime: 0.15 * (rCaseS * factMulti),
-      hospitalBedsByRequestedTime: Math.trunc(0.35 * input.totalHospitalBeds) - 0.15 * (rCaseS * factMulti),
-      casesForICUByRequestedTime: 0.05 * ((input.reportedCases * 50) * factMulti),
-      casesForVentilatorsByRequestedTime: 0.02 * ((input.reportedCases * 50) * factMulti),
-      dollarsInFlight: (((input.reportedCases * 50) * factMulti) * avgPIncome) * avgDIncome * pCal()
+      infectionsByRequestedTime: (input.reportedCases * 50) * fM,
+      severeCasesByRequestedTime: 0.15 * (rS * fM),
+      hospitalBedsByRequestedTime: Math.trunc(0.35 * input.totalHospitalBeds) - 0.15 * (rS * fM),
+      casesForICUByRequestedTime: 0.05 * ((input.reportedCases * 50) * fM),
+      casesForVentilatorsByRequestedTime: 0.02 * ((input.reportedCases * 50) * fM),
+      dollarsInFlight: (((input.reportedCases * 50) * fM) * avgPIncome) * avgDIncome * pCal()
     }
   };
 };
